7e7cfbea98df48adc383a5924592f54a41f1273f
Ar-Sibi <getgoingwithsibi@gmail.com>
2018-11-19 03:20:40 +0530

    NOV cookoff ,2 mediocre contests

---
+++ NOVCOOK/A.cpp
@@ -0,0 +1,130 @@
+#include<bits/stdc++.h>
+#include<unordered_map>
+using namespace std;
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<ll> vll;
+typedef long double LD;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pll> vpll;
+typedef pair<string,ll> psl;
+typedef vector<psl> vpsl;
+typedef vector<int> vi;
+
+#define fur(i,a,b) for(int (i)=(a);i<(b);++(i))
+#define revf(i,a,b) for(int (i)=(a)-1;(i)>=(b);--(i))
+#define mp make_pair
+#define mod 1000000007
+#define inf 2000000007
+#define pb push_back
+#define eps 1e-5
+#define X first
+#define Y second
+
+
+ll modpow(ll base, ll exp, ll modulus) {
+  base %= modulus;
+  ll result = 1;
+  while (exp > 0) {
+    if (exp & 1) result = (result * base) % modulus;
+    base = (base * base) % modulus;
+    exp >>= 1;
+  }
+  return result;
+}
+
+ll modInverse(ll a, ll m) 
+{ 
+    ll m0 = m; 
+    ll y = 0, x = 1; 
+  
+    if (m == 1) 
+      return 0; 
+  
+    while (a > 1) 
+    { 
+        // q is quotient 
+        ll q = a / m; 
+        ll t = m; 
+  
+        // m is remainder now, process same as 
+        // Euclid's algo 
+        m = a % m, a = t; 
+        t = y; 
+  
+        // Update y and x 
+        y = x - q * y; 
+        x = t; 
+    } 
+  
+    // Make x positive 
+    if (x < 0) 
+       x += m0; 
+  
+    return x; 
+} 
+
+
+int main(){
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+    ll t;
+    cin>>t;
+    while(t--){
+        ll tr;
+        cin>>tr;
+        unordered_map<ll,ll> rm;
+        fur(i,0,tr){
+            ll x;
+            cin>>x;
+            rm[x]=1;
+        }
+        ll ts;
+        cin>>ts;
+        unordered_map<ll,ll> sm;
+        fur(i,0,ts){
+            ll x;
+            cin>>x;
+            sm[x]=1;
+        }
+        ll fail=false;
+        ll tx;
+        cin>>tx;
+        fur(i,0,tx){
+            ll x;
+            cin>>x;
+            if(rm[x]!=1)
+            fail=true;
+        }
+        ll ty;
+        cin>>ty;
+        fur(i,0,ty){
+            ll x;
+            cin>>x;
+            if(sm[x]!=1)
+            fail=true;
+        }
+        if(!fail)
+        cout<<"yes"<<endl;
+        else
+        cout<<"no"<<endl;
+    }
+
+	
+	return 0;
+}
